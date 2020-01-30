4d1e9509b81c299726c45053e85d8a0dfc036176
Aditya Chakraborty <chakra17@purdue.edu>
2020-01-30 03:28:43 -0500

    Seperated video frame read function

--- scripts/test_video_resource.py
+++ scripts/test_video_resource.py
@@ -1,21 +1,32 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-
-Copyright (c) 2015 PAL Robotics SL.
-Released under the BSD License.
-
-Created on 7/14/15
-
-@author: Sammy Pfeiffer
-
-test_video_resource.py contains
-a testing code to see if opencv can open a video stream
-useful to debug if video_stream does not work
-"""
 
 import cv2
 import sys
+
+'''
+resource- file, webcam, server
+'''
+
+
+def get_video_frame(resource):
+        cap = cv2.VideoCapture(resource)
+        if not cap.isOpened():
+            print "Error opening resource: " + str(resource)
+            print "Maybe opencv VideoCapture can't open it"
+            exit(0)
+
+        print "Correctly opened resource, starting to show feed."
+        rval, frame = cap.read()
+        while rval:
+            cv2.imshow("Stream: " + resource_name, frame)
+            rval, frame = cap.read()
+            key = cv2.waitKey(20)
+
+            if key == 27 or key == 1048603:
+                break
+        cv2.destroyWindow("preview")
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -33,21 +44,4 @@ if __name__ == '__main__':
     else:
         resource_name = resource
     print "Trying to open resource: " + resource_name
-    cap = cv2.VideoCapture(resource)
-    if not cap.isOpened():
-        print "Error opening resource: " + str(resource)
-        print "Maybe opencv VideoCapture can't open it"
-        exit(0)
-
-    print "Correctly opened resource, starting to show feed."
-    rval, frame = cap.read()
-    while rval:
-        cv2.imshow("Stream: " + resource_name, frame)
-        rval, frame = cap.read()
-        key = cv2.waitKey(20)
-        # print "key pressed: " + str(key)
-        # exit on ESC, you may want to uncomment the print to know which key is ESC for you
-        if key == 27 or key == 1048603:
-            break
-    cv2.destroyWindow("preview")
-
+    get_video_frame(resource)
