9dd548c55cc848f8a6276986176c6de9d1b93c03
Aditya Chakraborty <chakra17@purdue.edu>
2020-03-03 23:30:26 -0500

    Added run rviz bash script and keyboard control python script

---
+++ non-package-scripts/run_rviz.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#roslaunch mybot_description mybot_rviz.launch
+#roslaunch boilerbot_description_integ mybot_rviz_amcl.launch 
+roslaunch boilerbot_description_integ mybot_rviz.launch 
